f65878d8803679539a806077433448f7e775cee5
Gjorgi03 <gorgiasprovski@gmail.com>
2024-10-18 18:35:16 +0200

    2024/25

--- Lecture3/src/Main.java
+++ Lecture3/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 6; i++) {
             System.out.println(i);
         }
     }
