27f86eb489a83b22dfa7a1d3b683e68a6b7c847b
Gjorgi03 <gorgiasprovski@gmail.com>
2024-10-25 01:38:45 +0200

    2024/25

---
+++ Learning/src/Class1.java
@@ -0,0 +1,9 @@
+public class Class1 {
+    int x = 2;
+
+    void printHI(){
+        System.out.println("hi");
+    }
+}
+
+
---
+++ Learning/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Class1 c = new Class1();
+        Class1 c2 = new Class1();
+        Class2 c3 = new Class2();
+        System.out.println(c.x);
+        c2.printHI();
+        System.out.println(c3.y);
+    }
+}
